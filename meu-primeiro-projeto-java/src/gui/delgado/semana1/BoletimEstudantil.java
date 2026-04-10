d74a41373ddc7054bc9561f652a2aac18df2c590
guizi1 <luisguidvg1225@gmail.com>
2026-04-09 21:36:45 -0300

    Update mediaFinal value in BoletimEstudantil to reflect passing criteria

--- meu-primeiro-projeto-java/src/gui/delgado/semana1/BoletimEstudantil.java
+++ meu-primeiro-projeto-java/src/gui/delgado/semana1/BoletimEstudantil.java
@@ -3,7 +3,7 @@ package gui.delgado.semana1;
 public class BoletimEstudantil {
     public static void main(String[] args){
 
-        int mediaFinal = 7;
+        int mediaFinal = 4;
         
         if(mediaFinal < 6)
             System.out.println("Reprovado");
