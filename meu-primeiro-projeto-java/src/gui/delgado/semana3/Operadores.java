fd8ebfa15afcf233f9baf6d06a05b0e5cd8734c4
guizi1 <luisguidvg1225@gmail.com>
2026-04-10 20:39:05 -0300

    commit

--- meu-primeiro-projeto-java/src/gui/delgado/semana3/Operadores.java
+++ meu-primeiro-projeto-java/src/gui/delgado/semana3/Operadores.java
@@ -1,32 +1,41 @@
+package gui.delgado.semana3;
+
 import java.util.Scanner;
 
-public class Operadores{
-    public static void main(String[] args){
-
+public class Operadores {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
         double valorA;
         double valorB;
         double valorC;
-        /// Valor A
-        try (Scanner scanner = new Scanner(System.in)) {
-            /// Valor A
-            System.out.print("Digite o valor de a: ");
-            valorA = scanner.nextDouble();
-            /// Valor B
-            System.out.print("Digite o valor de b: ");
-            valorB = scanner.nextDouble();
-            /// Valor C
-            System.out.print("Digite o valor de c: ");
-            valorC = scanner.nextDouble();
-        }
+        
+        System.out.print("Digite o valor de a: ");
+        valorA = scanner.nextDouble();
+        
+        System.out.print("Digite o valor de b: ");
+        valorB = scanner.nextDouble();
+        
+        System.out.print("Digite o valor de c: ");
+        valorC = scanner.nextDouble();
+        
+        double delta = valorDelta(valorA, valorB, valorC);
+        System.out.println("Valor de delta: " + delta);
 
-        ///Validação dos valores
-        System.out.println("Valor de A:");
-        System.out.println(valorA);
-        System.out.println("Valor de B:");
-        System.out.println(valorB);
-        System.out.println("Valor de C:");
-        System.out.println(valorC);
+        double raizDelta = Math.sqrt(delta);
+        double x1 = ((valorB * -1) + raizDelta) / (2 * valorA);
+        double x2 = ((valorB * -1) - raizDelta) / (2 * valorA);
+        System.out.println("Valor de x1 = " + x1 + "\nValor de x2 = " + x2);
 
+        scanner.close();
+    }
+
+    public static double valorDelta (double valorA, double valorB, double valorC){
+        return (valorB * valorB) - 4 * valorA * valorC;
+    }
+
+    public static double calcularBhaskara (double valorA, double valorB, double delta){
+        return (valorB * -1) + Math.sqrt(delta) / 2 * valorA;
     }
 
 }
