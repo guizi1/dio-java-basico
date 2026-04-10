b633e382aa17d6cbc5cd94bacf097e6cc6e5013b
guizi1 <luisguidvg1225@gmail.com>
2026-04-09 21:07:39 -0300

    commit

---
+++ meu-primeiro-projeto-java/src/MinhaClasse.java
@@ -0,0 +1,16 @@
+public class MinhaClasse {
+
+    public static void main (String [] args) {
+
+        String primeiroNome = "Luis";
+        String segundoNome = "Delgado";
+        String nomeCompleto = nomeCompleto (primeiroNome, segundoNome);
+        System.out.println(nomeCompleto);
+
+    }
+
+    public static String nomeCompleto (String primeiroNome, String segundoNome){
+        return "Resultado do metodo ".concat(primeiroNome).concat(" ").concat(segundoNome);
+    }
+
+}
